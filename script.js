a598b6bf9aeab3192310ad43651976577ca39985
Lokesh Rawat <98968993+LokeshRawat07@users.noreply.github.com>
2023-04-26 17:20:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// "use script";
+
+// const bgElement = document.querySelector(".header__bg");
+// const menu = document.querySelector(".header__menu");
+// const navbar = document.querySelector(".navbar");
+
+// const hamTop = document.querySelector(".header__hamburger-top");
+// const hamBottom = document.querySelector(".header__hamburger-bottom");
+// const hamMain=document.querySelector('.header__hamburger-main');
+
+// menu.addEventListener("click", () => {
+//   bgElement.classList.toggle("header__bg--open");
+//   navbar.classList.toggle("navbar--show");
+
+//   hamTop.classList.toggle("header__hamburger-top--hide");
+//   hamBottom.classList.toggle("header__hamburger-bottom--hide");
+//   hamMain.classList.toggle('header__hamburger-main--active');
+
+  //   if (bgElement.classList.contains("header__bg--open")) {
+  //     bgElement.classList.remove("header__bg--open");
+  //     navbar.classList.remove("navbar--show");
+
+  //     hamTop.classList.remove("header__hamburger-top--hide");
+  //     hamBottom.classList.remove("header__hamburger-bottom--hide");
+  //   } else {
+  //     bgElement.classList.add("header__bg--open");
+  //     navbar.classList.add("navbar--show");
+
+  //     //making the hamburger line animate
+  //     hamTop.classList.add("header__hamburger-top--hide");
+  //     hamBottom.classList.add("header__hamburger-bottom--hide");
+  //   }
+});
